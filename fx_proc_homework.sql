5bccf3be153ab526170578cbc673fac4b505078e
rebeccalpeace <rebeccalpeace@gmail.com>
2022-08-12 10:51:53 -0400

    fixed rating

--- fx_proc_homework.sql
+++ fx_proc_homework.sql
@@ -2,12 +2,22 @@
 -- create a stored procedure that will insert a new film into the film table with the following arguments:
 -- title, description, release_year, language_id, rental_duration, rental_rate, length, replace_cost, rating
 
-CREATE OR REPLACE PROCEDURE add_film(title VARCHAR(255), description VARCHAR, release_year INTEGER, language_id INTEGER, rental_duration INTEGER, rental_rate NUMERIC(4,2), length INTEGER, replacement_cost NUMERIC(5,2))
+CREATE OR REPLACE PROCEDURE add_film(
+	title VARCHAR(255), 
+	description VARCHAR, 
+	release_year INTEGER, 
+	language_id INTEGER, 
+	rental_duration INTEGER, 
+	rental_rate NUMERIC(4,2), 
+	length INTEGER, 
+	replacement_cost NUMERIC(5,2),
+	rating mpaa_rating
+)
 LANGUAGE plpgsql
 AS $$
 BEGIN 
-	INSERT INTO film(title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost)
-	VALUES (title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost);
+	INSERT INTO film(title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost, rating)
+	VALUES (title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost, rating);
 END;
 $$
 
@@ -15,9 +25,10 @@ SELECT *
 FROM film
 ORDER BY title DESC;
 
-CALL add_film('Adaptation', 'A Movie About Orchids and Writing That Will Probably Make You Feel Weird', 2000, 1, 5, 4.99, 100, 19.99);
+CALL add_film('Adaptation', 'A Movie About Orchids and Writing That Will Probably Make You Feel Weird', 2000, 1, 5, 4.99, 100, 19.99, 'R');
 
-CALL add_film('Zoo Animals', 'A Movie About Zoo Animals', 1995, 1, 4, 5.99, 130, 14.99);
+CALL add_film('Zoo Animals', 'A Movie About Zoo Animals', 1995, 1, 4, 5.99, 130, 14.99, 'G');
+
 
 
 -- Question 2
